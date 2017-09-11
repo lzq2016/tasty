b6ed86daa152d4a84d9142ae88eedcb68b98e7fa
lizhanqiang@kanzhun.com <lizhanqiang@kanzhun.com>
2017-09-11 14:02:31 +0800

    lzq

--- src/pages/find/find.js
+++ src/pages/find/find.js
@@ -1,10 +1,28 @@
 
 
 Page({
-  data: {
-    
-  },
-  onLoad: function () {
-    
-  }
+    data: {
+        list: [],
+        array: [{
+            message: 'foo',
+        }, {
+            message: 'bar'
+        }]
+    },
+    onLoad: function () {
+        var self = this;
+        wx.request({
+            url: 'https://www.sharetasty.com:8443/client/CommunityService/searchHotTopic',
+            data: {
+                pageNum: 0,
+                pageCount: 1
+            },
+            success: function (res) {
+                console.log(res.data)
+                if (res.data.resultstate == 1) {
+                    self.setData({list:res.data.result});
+                }
+            }
+        })
+    }
 })
